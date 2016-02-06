276139cda7697bae45f08319a224822225ff9221
Atush <maria.artemieva@mail.ru>
2016-02-06 16:27:42 +0300

    Contact class is added

---
+++ contact.py
@@ -0,0 +1,18 @@
+class Contact:
+    
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, homephone, mobile, workphone, fax, email, email2, email3, homepage):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homephone = homephone
+        self.mobile = mobile
+        self.workphone = workphone
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
