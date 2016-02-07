db5b07adc3e56934cc5cb2650a3902dfe190c5d5
Atush <maria.artemieva@mail.ru>
2016-02-07 17:02:39 +0300

    Packages are created for different areas of responsibilities. SessionHelper class is added.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from contact import Contact
-from application import Application
+from model.contact import Contact
+from fixture.application import Application
 
 @pytest.fixture
 def app(request):
@@ -11,11 +11,11 @@ def app(request):
 
 def test_test_add_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="Ivan", middlename="Ivanych", lastname="Petrov", nickname="petya",
                                     title="testing3", company="gasprom", address="NY", homephone="sweet home",
                                     mobile="1234567", workphone="7654321", fax="1234", email="email@email.ru", email2="email2@email.ru",
                                     email3="email3@email.ru", homepage="index.ru", bday="17", bmonth="March",
                                     byear="1990", aday="27", amonth="September", ayear="2000",
                                     address2="Soviet Union", phone2="sweet-sweet home", notes="be prepared"))
-    app.logout()
+    app.session.logout()
