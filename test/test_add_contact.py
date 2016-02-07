fc51b6860d49ce8c8b431e67268ea16fe9401cf0
Atush <maria.artemieva@mail.ru>
2016-02-07 21:42:17 +0300

    ContactHelper class is added

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -9,13 +9,13 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
-def test_test_add_contact(app):
+def test_add_contact(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="Ivan", middlename="Ivanych", lastname="Petrov", nickname="petya",
-                                    title="testing3", company="gasprom", address="NY", homephone="sweet home",
-                                    mobile="1234567", workphone="7654321", fax="1234", email="email@email.ru", email2="email2@email.ru",
-                                    email3="email3@email.ru", homepage="index.ru", bday="17", bmonth="March",
-                                    byear="1990", aday="27", amonth="September", ayear="2000",
-                                    address2="Soviet Union", phone2="sweet-sweet home", notes="be prepared"))
+    app.contact.create(Contact(firstname="Ivan", middlename="Ivanych", lastname="Petrov", nickname="petya",
+                               title="testing3", company="gasprom", address="NY", homephone="sweet home",
+                               mobile="1234567", workphone="7654321", fax="1234", email="email@email.ru", email2="email2@email.ru",
+                               email3="email3@email.ru", homepage="index.ru", bday="17", bmonth="March",
+                               byear="1990", aday="27", amonth="September", ayear="2000",
+                               address2="Soviet Union", phone2="sweet-sweet home", notes="be prepared"))
     app.session.logout()
