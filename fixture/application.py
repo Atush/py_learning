f5c816bd82a9c3a8ab916fe757fec5d40fdacf72
Atush <maria.artemieva@mail.ru>
2016-03-08 22:25:31 +0300

    Now I can set browser and base_url from terminal

--- fixture/application.py
+++ fixture/application.py
@@ -1,13 +1,22 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.manager import ManageHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.base_url = base_url
         self.open_home_page()
         self.wd.implicitly_wait(3)
         self.manager = ManageHelper(self)
+
 
 
     def is_valid(self):
@@ -20,7 +29,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
+        wd.get(self.base_url)
 
 
     def destroy(self):
