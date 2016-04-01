2c25151882eb0fc8864fd868cf20d04311d2bac7
Atush <maria.artemieva@mail.ru>
2016-04-01 17:50:23 +0300

    Now we compare tha data on home page with the data in db.

--- test/test_contact_data.py
+++ test/test_contact_data.py
@@ -1,17 +1,18 @@
 import re
-from random import randrange
+from fixture.contact import Contact
 
 
-def test_all_data_on_home_page(app):
-    contact_list = app.contact.get_contact_list()
-    index = randrange(len(contact_list))
-    contact_from_home_page = contact_list[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+def test_all_data_on_home_page(app, db):
+    contact_list_from_ui = sorted(app.contact.get_contact_list(), key=Contact.id_con_max)
+    contact_list_from_db = sorted(db.get_contact_list(), key=Contact.id_con_max)
+    i=0
+    for contact_ui in contact_list_from_ui:
+        print(str(contact_ui.all_emails_from_home_page) + ' and finally ' + str(merge_emails_like_on_home_page(contact_list_from_db[i])))
+        assert contact_ui.firstname == contact_list_from_db[i].firstname.strip()
+        assert contact_ui.lastname == contact_list_from_db[i].lastname.strip()
+        assert contact_ui.all_phones_from_home_page == merge_phones_like_on_home_page(contact_list_from_db[i])
+        assert contact_ui.all_emails_from_home_page == merge_emails_like_on_home_page(contact_list_from_db[i])
+        i=i+1
 
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
